936a2eb30dcb0d606591b232a04d0a1a81734a94
Yevhen <jberestian@gmail.com>
2022-12-12 14:47:33 +0200

    home work

--- practice-part-1/script.js
+++ practice-part-1/script.js
@@ -64,7 +64,52 @@
 
 // firstTask();
 
-// Место для второй задачи
-function secondTask() {
-  // Пишем решение вот тут
+// // Место для второй задачи
+// function secondTask() {
+//   for (let i = 20; i >= 10; i--) {
+//     if (i === 13) {
+//       break;
+//     }
+//     console.log(i);
+//   }
+//   // Пишем решение вот тут
+// }
+
+// secondTask();
+
+// // Место для третьей задачи
+// function thirdTask() {
+//   for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+//   // Пишем решение вот тут
+// }
+// thirdTask();
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+function fourTask() {
+  let i = 2;
+  while (i <= 16) {
+    if (i % 2 === 0) {
+      i++;
+      continue;
+    } else {
+      console.log(i);
+    }
+    i++;
+  }
 }
+fourTask();

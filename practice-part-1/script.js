2195224698aaccaca4ae462a39d5790f5d0986a5
Yevhen <jberestian@gmail.com>
2022-12-12 14:56:56 +0200

    added tasks

--- practice-part-1/script.js
+++ practice-part-1/script.js
@@ -113,3 +113,16 @@ function fourTask() {
   }
 }
 fourTask();
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+  }
+
+  console.log(arrayOfNumbers);
+  return arrayOfNumbers;
+}
+
+fifthTask();
